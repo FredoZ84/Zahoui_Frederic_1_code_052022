b87c30ca811e0bf04c43863573b43932c413ed26
FredoZ84 <frederic.zahoui@gmail.com>
2022-05-18 08:46:39 +0200

    Optimisation n°2 du fichier formValidation.js

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,27 +1,30 @@
-let firstName = document.getElementById("first"),
-    lastName = document.getElementById("last"),
-    email = document.getElementById("email"),
-    birthdate = document.getElementById("birthdate"),
-    participationNumber = document.getElementById("quantity"),
-    locations = document.getElementById('locations'),
-    termsOfUse = document.getElementById('checkbox1');           
-       
+let     Input = {
+    firstName: document.getElementById("first"),
+    lastName: document.getElementById("last"),
+    email: document.getElementById("email"),
+    birthdate: document.getElementById("birthdate"),
+    participationNumber: document.getElementById("quantity"),
+    locations: document.getElementById('locations'),
+    termsOfUse: document.getElementById('checkbox1')            
+}        
 
-const form = document.getElementById("form"),
-    confirmation = document.getElementById("confirmation"),
-    Regex = {
-        name: /^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/,
-        mail: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        date: /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/
-    };
+const   form = document.getElementById("form"),
+confirmation = document.getElementById("confirmation"),
+Regex = {
+    name: /^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/,
+    mail: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    date: /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/
+};
 
-firstName.addEventListener("focusout", checkName);
-lastName.addEventListener("focusout", checkName);
-email.addEventListener("focusout", checkMail );
-birthdate.addEventListener("focusout", checkBirthdate);
-participationNumber.addEventListener("focusout", checkTournamentsQuantity);
-locations.addEventListener("change", checkLocations);
-termsOfUse.addEventListener("change",checkCheckBox);
+
+for (let property in Input) {
+    if ([property] !== locations || [property] !== termsOfUse ) {
+        Input[property].addEventListener("focusout", check);
+    }
+}
+
+Input.locations.addEventListener("change", check);
+Input.termsOfUse.addEventListener("change", check);
 
 // choice of condition of check function
 function conditionOfCheck(condition) {// put the negative condition
@@ -32,47 +35,40 @@ function conditionOfCheck(condition) {// put the negative condition
     }
 }
 
-function checkName(e) {
+// Element verification
+function check(e) {    
     let toTest = e.target.value,
+        type = e.target.type,
+        element = e.target,
         result;
     
-    result = conditionOfCheck(toTest.length < 2 || toTest ===" "|| !toTest.match(Regex.name));
 
-    console.log(result);
-
-    return result;
-}
-
-function checkMail(e) {
-    let toTest = e.target.value,
-        result;
+    // en fonction du de l'input on vérifie si la saisie est juste
+    switch (type) {
+        case "text": // for firstname & lastname
+            result = conditionOfCheck(toTest.length < 2 || toTest ===" " || !toTest.match(Regex.name));
+        break;
+        case "email":              
+            result = conditionOfCheck(!toTest.match(Regex.mail));
+        break;
+        case "date":
+            result = conditionOfCheck(!toTest.match(Regex.date)); 
+        break;
+        case "number":
+            result = conditionOfCheck(toTest.length === 0 || isNaN(toTest) === true || toTest < 0|| toTest > 99);
+        break;
+        case "radio":
+            result = checkLocations(e);
+        break;
+        case "checkbox":
+            result = checkCheckBox(e);
+        break;
         
-    result = conditionOfCheck(!toTest.match(Regex.mail)); 
-    
-    console.log(result);
+        default:
+              console.log('Erreur de condition');
+    }
 
-    return result;
-}
-
-
-function checkBirthdate(e) {
-    let toTest = e.target.value,
-    result;
-
-    result = conditionOfCheck(toTest.length !== 10);
-
-    console.log(result);
-
-    return result;
-}
-
-function checkTournamentsQuantity(e) {
-    let toTest = e.target.value,
-    result;
-
-    result = conditionOfCheck(toTest.length === 0 || isNaN(toTest) === true || toTest < 0|| toTest > 99);
-
-    console.log(result);
+    console.log(result)
 
     return result;
 }
@@ -92,8 +88,6 @@ function checkLocations(e) {
     
     result = conditionOfCheck(selectedSearch !== 1);    
 
-    console.log(result);
-
     return result;
 }
 
@@ -102,8 +96,6 @@ function checkCheckBox(e) {
         result;
 
     result = conditionOfCheck(!toTest);
-
-    console.log(result);
 
     return result;
 }
