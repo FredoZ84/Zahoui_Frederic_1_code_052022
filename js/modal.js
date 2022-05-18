cd68df79b550aabf066d187a7449814df7e3f834
FredoZ84 <frederic.zahoui@gmail.com>
2022-05-18 08:43:07 +0200

    Elaboration de la fermeture du modal (modal.js)

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,4 @@
+// Affichage du menu de navigation lors des formats tablettes et mobiles
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -9,15 +10,35 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+const modalContent = document.querySelector(".content");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModalBtn = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// close modal event
+closeModalBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// close modal
+function closeModal() {
+  modalContent.animate([
+    // step/keyframes
+    {opacity: "0"}
+    
+  ], {
+    // temporisation
+    duration: 1000
+  });
+  setTimeout(function(){
+    modalbg.style.display = "none";
+  },900);  
+  if (sessionStorage.getItem("validate")) {
+    window.location.reload();
+  }
+}
